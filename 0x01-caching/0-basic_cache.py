be7860e667e836dfb88ae4720e2442b77fbb90ca
Abdelhay1212 <110763003+Abdelhay1212@users.noreply.github.com>
2024-02-01 05:41:05 +0000

    init the project and solved the first task

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+''' Basic dictionary '''
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class BasicCache(BaseCaching):
+    ''' basic cache '''
+
+    def __init__(self):
+        super().__init__()
+
+    def put(self, key, item):
+        ''' add item into the cache '''
+        if key is not None or item is not None:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        if key is not None and key in self.cache_data:
+            return self.cache_data[key]
+        return None
